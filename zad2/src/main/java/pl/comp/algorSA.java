d1162808bbabf54d1c6e15f4fbc327180eaf7f4e
Edyta Szymańska <251647@edu.p.lodz.pl>
2025-10-27 12:58:29 +0100

    Initial commit

---
+++ zad2/src/main/java/pl/comp/algorSA.java
@@ -0,0 +1,22 @@
+package pl.comp;
+
+public class algorSA {
+
+    private float T0;
+    private int alfa;
+    private int M;
+    private int k;
+
+    public algorSA(float t0, int alfa, int m, int k) {
+        T0 = t0;
+        this.alfa = alfa;
+        M = m;
+        this.k = k;
+    }
+
+    public void algorithm(){
+        for (int i = 0; i <= M; i++) {
+            //function
+        }
+    }
+}
