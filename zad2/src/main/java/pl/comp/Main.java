a4295c250af04fa94dbb1147cff99541128702d2
251606 Pankanin Wiktor <251606@edu.p.lodz.pl>
2025-11-06 15:27:39 +0100

    dodanie 2 funkjci do podpunktu 3

--- zad2/src/main/java/pl/comp/Main.java
+++ zad2/src/main/java/pl/comp/Main.java
@@ -39,7 +39,10 @@ public class Main {
 
         // === CZĘŚĆ 2: Analiza wpływu parametrów ===
         System.out.println("\n\n=== Analiza wpływu parametrów (1D) ===");
-        analyzeParameters1D(x1_1D, x2_1D, expectedMax1D, expectedX1D, k_1D);
+        analyzeParameters1D(x1_1D, x2_1D, expectedMax1D, k_1D);
+
+        System.out.println("\n\n=== Analiza wpływu parametrów (2D) ===");
+        analyzeParameters2D(x1_2D, x2_2D, y1_2D, y2_2D, expectedMax1D, k_1D);
     }
 
     // --- eksperymenty ---
@@ -82,7 +85,7 @@ public class Main {
     }
 
     // --- ANALIZA WPŁYWU PARAMETRÓW ---
-    private static void analyzeParameters1D(double x1, double x2, double expectedMax, double expectedX, double k) {
+    private static void analyzeParameters1D(double x1, double x2, double expectedMax, double k) {
         double[] T0_values = {1, 5, 50, 100};
         double[] alpha_values = {0.9, 0.95, 0.99, 0.999};
         int[] M_values = {100, 500, 1000};
@@ -96,7 +99,7 @@ public class Main {
                     List<Double> wyniki = new ArrayList<>();
                     long totalTime = 0;
 
-                    totalTime = getTotalTime(x1, x2, k, T0, alpha, M, wyniki, totalTime);
+                    totalTime = getTotalTime1D(x1, x2, k, T0, alpha, M, wyniki, totalTime);
 
                     double avgF = srednia(wyniki);
                     double stdF = odchylenie(wyniki, avgF);
@@ -110,7 +113,7 @@ public class Main {
         }
     }
 
-    private static long getTotalTime(double x1, double x2, double k, double T0, double alpha, int M, List<Double> wyniki, long totalTime) {
+    private static long getTotalTime1D(double x1, double x2, double k, double T0, double alpha, int M, List<Double> wyniki, long totalTime) {
         for (int run = 0; run < 5; run++) {
             long start = System.nanoTime();
             algorSA sa = new algorSA(T0, alpha, M, k);
@@ -123,6 +126,48 @@ public class Main {
         }
         return totalTime;
     }
+
+    private static void analyzeParameters2D(double x1, double x2, double y1, double y2,double expectedMax, double k){
+        double[] T0_values = {1, 5, 50, 100};
+        double[] alpha_values = {0.9, 0.95, 0.99, 0.999};
+        int[] M_values = {100, 500, 1000};
+
+        System.out.printf("%8s %8s %8s %15s %15s %15s %15s%n",
+                "T0", "alpha", "M", "Śr. czas [ms]", "Śr. f(x,y)", "Śr. |Δf|", "Odchylenie");
+        for (double T0 : T0_values) {
+            for (double alpha : alpha_values) {
+                for (int M : M_values) {
+                    List<Double> wyniki = new ArrayList<>();
+                    long totalTime = 0;
+
+                    totalTime = getTotalTime2D(x1, x2, y1, y2, k, T0, alpha, M, wyniki, totalTime);
+
+                    double avgF = srednia(wyniki);
+                    double stdF = odchylenie(wyniki, avgF);
+                    double avgTime = (totalTime / 5.0) / 1_000_000.0; // w ms
+                    double diff = Math.abs(expectedMax - avgF);
+
+                    System.out.printf("%8.1f %8.3f %8d %15.2f %15.5f %15.5f %15.5f%n",
+                            T0, alpha, M, avgTime, avgF, diff, stdF);
+                }
+            }
+        }
+    }
+
+    private static long getTotalTime2D(double x1, double x2, double y1, double y2, double k, double T0, double alpha, int M, List<Double> wyniki, long totalTime) {
+        for (int run = 0; run < 5; run++) {
+            long start = System.nanoTime();
+            algorSA sa = new algorSA(T0, alpha, M, k);
+            double[] result = sa.algorithm(x1, x2, y1, y2);
+            double resultVal = sa.func(result[0], result[1]);
+            long end = System.nanoTime();
+
+            wyniki.add(resultVal);
+            totalTime += (end - start);
+        }
+        return totalTime;
+    }
+
 
     private static double srednia(List<Double> arr) {
         double s = 0;
